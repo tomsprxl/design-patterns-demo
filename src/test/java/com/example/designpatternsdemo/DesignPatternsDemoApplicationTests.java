5f6f4a38c77fd5b22ca20c0224bf569f241f5839
xuliang <xuliang@efunong.com>
2019-09-09 15:43:14 +0800

    add file

---
+++ src/test/java/com/example/designpatternsdemo/DesignPatternsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.designpatternsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesignPatternsDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
